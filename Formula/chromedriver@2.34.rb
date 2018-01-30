99ff313a0c3a2ea887bc73c44355b035e366b940
Alan Fachini <alfakini@gmail.com>
2018-01-30 10:45:19 -0200

    chromedriver@2.34 (formula version)

---
+++ Formula/chromedriver@2.34.rb
@@ -0,0 +1,50 @@
+class ChromedriverAT234 < Formula
+  desc "Tool for automated testing of webapps across many browsers"
+  homepage "https://sites.google.com/a/chromium.org/chromedriver/"
+  url "https://chromedriver.storage.googleapis.com/2.34/chromedriver_mac64.zip"
+  version "2.34"
+  sha256 "bc4ad903c1f85ed328726a9914fbec9d539be8eda0a5489e8dc5dd08123d28fc"
+
+  bottle :unneeded
+
+  def install
+    bin.install "chromedriver"
+  end
+
+  plist_options :manual => "chromedriver"
+
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>Label</key>
+      <string>homebrew.mxcl.chromedriver</string>
+      <key>RunAtLoad</key>
+      <true/>
+      <key>KeepAlive</key>
+      <false/>
+      <key>ProgramArguments</key>
+      <array>
+        <string>#{opt_bin}/chromedriver</string>
+      </array>
+      <key>ServiceDescription</key>
+      <string>Chrome Driver</string>
+      <key>StandardErrorPath</key>
+      <string>#{var}/log/chromedriver-error.log</string>
+      <key>StandardOutPath</key>
+      <string>#{var}/log/chromedriver-output.log</string>
+    </dict>
+    </plist>
+    EOS
+  end
+
+  test do
+    driver = fork do
+      exec bin/"chromedriver", "--port=9999", "--log-path=#{testpath}/cd.log"
+    end
+    sleep 5
+    Process.kill("TERM", driver)
+    assert_predicate testpath/"cd.log", :exist?
+  end
+end
--- README.md
+++ README.md
@@ -20,7 +20,8 @@ $ brew tap magrathealabs/homebrew-mlabs
 Once the tap is installed, you can install the packages using the `brew install` command:
 
 ```sh
-$ brew install icu4c@58.1
+brew install chromedriver@2.35
+brew install icu4c@58.1
 ```
 
 That's it!
@@ -28,6 +29,7 @@ That's it!
 Please also follow the instructions from `brew info` at the end of the install to ensure you properly installed the package.
 
 ## Troubleshooting
+
 First, please run `brew update` (twice) and `brew doctor`.
 
 Second, read the [Troubleshooting Checklist](https://docs.brew.sh/Troubleshooting.html).
@@ -35,6 +37,7 @@ Second, read the [Troubleshooting Checklist](https://docs.brew.sh/Troubleshootin
 **If you don’t read these it will take us far longer to help you with your problem.**
 
 ## Homebrew documentation
+
 `brew help`, `man brew`, [Homebrew/brew's README](https://github.com/Homebrew/brew#homebrew) or check [Homebrew's documentation](https://github.com/Homebrew/brew/tree/master/docs#readme).
 
 ## License
